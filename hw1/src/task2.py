3d96736c23192ee9702bdcfc0dc50d3ad4db7a15
ofaclaude <ofa.claude12@proton.me>
2026-09-23 11:10:56 -0600

    task 2

---
+++ hw1/src/task2.py
@@ -0,0 +1,11 @@
+def produce_int():
+    return 1
+
+def produce_float():
+    return 1.1
+
+def produce_string():
+    return "string"
+
+def produce_bool():
+    return True
---
+++ hw1/tests/test_task2.py
@@ -0,0 +1,13 @@
+from hw1.src.task2 import produce_bool, produce_float, produce_int, produce_string
+
+def test_produce_int():
+    assert type(produce_int()) == int
+
+def test_produce_int():
+    assert type(produce_float()) == float
+
+def test_produce_int():
+    assert type(produce_string()) == str
+
+def test_produce_int():
+    assert type(produce_bool()) == bool
