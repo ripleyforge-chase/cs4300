757c05a07f69fc5d081d5b20422bbf6b5faadbf3
ofaclaude <ofa.claude12@proton.me>
2026-09-23 11:52:41 -0600

    task 5

---
+++ hw1/src/task5.py
@@ -0,0 +1,30 @@
+def book_list():
+    books = [
+        "To Kill a Mocking Bird by Harper Lee",
+        "Pride and Prejudice by Jane Austen",
+        "The Great Gatsby by F. Scott Fitzgerald",
+        "1984 by George Orwell",
+        "Frankenstein by Mary Shelley",
+        "The Count of Monte Cristo by Alexandre Dumas",
+        "To the Lighthouse by Virginia Woolf"
+        ]
+
+    return books[0:3]
+
+def student_dict():
+    students = {
+        1001 : "Chase",
+        1002 : "Seth",
+        1003 : "Fabian",
+        1004 : "Tom",
+        1005 : "Fable",
+        1006 : "Opus",
+        1007 : "Sonnet",
+        1008 : "Haiku",
+        1009 : "Astra",
+        1010 : "Sol",
+        1011 : "Terra",
+        1012 : "Luna"
+    }
+
+    return students
---
+++ hw1/tests/test_task5.py
@@ -0,0 +1,16 @@
+from hw1.src.task5 import book_list, student_dict
+
+def test_books():
+    assert book_list() == ["To Kill a Mocking Bird by Harper Lee", "Pride and Prejudice by Jane Austen", "The Great Gatsby by F. Scott Fitzgerald"]
+
+def test_student1():
+    students = student_dict()
+    assert students.get(1001) == "Chase"
+
+def test_student5():
+    students = student_dict()
+    assert students.get(1005) == "Fable"
+
+def test_student10():
+    students = student_dict()
+    assert students.get(1010) == "Sol"
