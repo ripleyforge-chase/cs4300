5d5ae7662d9034007ea2daba83337a44cdf8cfec
ofaclaude <ofa.claude12@proton.me>
2026-09-23 11:05:26 -0600

    task 1

--- hw1/src/task1.py
+++ hw1/src/task1.py
@@ -1,1 +1,2 @@
-print("Hello World!")
+def hello_world():
+    print("Hello World!")
---
+++ hw1/tests/test_task1.py
@@ -0,0 +1,7 @@
+from hw1.src.task1 import hello_world
+
+def test_hello_world(capsys):
+    hello_world()
+
+    captured = capsys.readouterr()
+    assert captured.out == "Hello World!\n"
